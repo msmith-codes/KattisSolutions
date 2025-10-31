094af2ff6cf6061ca24ebbc00c670b3e4e896b35
msmith-codes <info@msmith.codes>
2025-10-31 17:38:24 -0400

    WOOOOO

---
+++ StalinRanking/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    int n = 0;
+    int x = 0;
+    std::vector<int> a;
+
+    std::cin >> n;
+
+    bool is_sorted = false;
+    
+    // Inputing data.
+    for(int i = 0; i < n; ++i) {
+        int temp = 0;
+        std::cin >> temp;
+        a.push_back(temp);
+    }
+
+    if(std::is_sorted(a.begin(), a.end(), std::greater<int>())) {
+        std::cout << a.size() << std::endl;
+        return 0;
+    }
+
+
+    int c = 0;
+
+    while(!a.empty()) {
+        int max_int = 0;
+
+        for(int i = 0; i < a.size(); ++i) {
+            if(a.at(i) >= max_int) {
+                max_int = a.at(i);
+                a[i] = -1;
+                ++c;
+            }
+        }
+
+        std::erase(a, -1);
+
+        ++x;
+    }
+    
+    
+    std::cout << x << std::endl;
+
+    return 0;
+}
